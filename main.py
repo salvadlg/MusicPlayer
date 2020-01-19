4a56620b6a591ce0f5f5905e5a9615c5ce74f8e5
salvadlg <50143387+salvadlg@users.noreply.github.com>
2020-01-19 15:54:03 +0100

    Add files via upload
    Add pause and stop button

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from tkinter import *
+from pygame import mixer
 
 root = Tk()
+
+mixer.init()
+
 root.geometry('700x400')
 root.title("Music Player")
-root.iconbitmap("headphone.gif")
 
 print(TkVersion)
 
-photo = PhotoImage(file="play.gif")
-btn = Button(root, image=photo)
-btn.pack()
+def play_music():
+    mixer.music.load("nombrecancion.wav")
+    mixer.music.play()
 
+playphoto = PhotoImage(file="play.png")
+playbtn = Button(root, image=playphoto)
+playbtn.pack()
+
+stopphoto = PhotoImage(file = "stop.png")
+stopbtn = Button(root, image=stopphoto)
+stopbtn.pack()
+
+pausephoto = PhotoImage(file = "pause.png")
+pausebtn = Button(root, image=pausephoto)
+pausebtn.pack()
 
 root.mainloop()
